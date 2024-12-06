01de4e97d2c93d809f04d7720dcc25ae4551b68b
Nurudeen Durowade <Nurudeen.durowade@wemabank.com>
2024-12-06 23:42:53 +0100

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of Mvc Mastery",
+                Id = 1,
+                DateOfBirth = new DateTime(1997,12,04)
+
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+       
+
+        public string? Name { get; set; }
+
+        public int Id { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
